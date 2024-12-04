3ddd0946a262295ad9f1b072698e204c80d1fc49
savad k <124030718+S4vad@users.noreply.github.com>
2024-12-04 20:22:18 +0530

    projectSetup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserLogin from './components/Auth/UserLogin'
+
+const App = () => {
+  return (
+    <>
+      <UserLogin />
+    </>
+  )
+}
+
+export default App
