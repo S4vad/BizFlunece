5173ce8833eab6cfe166ffdb18d2ca28446a8dfb
savad k <124030718+S4vad@users.noreply.github.com>
2025-03-04 12:21:09 +0530

    authcontext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,43 @@ import Dashboard from "./pages/business/Dashboard";
 import UserLogin from "./components/UserLogin";
 import Campaign from "./pages/Business/Campaign";
 import Profile from "./pages/influencer/Profile";
+import userRoutes from "./components/protectedRoute";
+import { AuthProvider } from "./context/AuthContext";
+import userRoutes from "./routes/userRoutes";
+import BusinessRoutes from "./routes/BusinessRoutes";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/login" element={<UserLogin />} />
-      <Route path="/campaign" element={<Campaign />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/campaign" element={<Campaign />} />
+        <Route path="/profile" element={<Profile />} />
+
+        <Route
+          path="/influencer/*"
+          element={
+            <protectedRoute role="influencer">
+              <userRoutes />
+            </protectedRoute>
+          }
+        />
+
+        {/* <Route element={<protectedRoute role="influcnecer" />}>
+          <userRoutes /> this is an old way but v6 not support withoout path
+        </Route> */}
+        <Route
+          path="/business/*"
+          element={
+            <protectedRoute role="business">
+              <BusinessRoutes />
+            </protectedRoute>
+          }
+        />
+      </Routes>
+    </AuthProvider>
   );
 }
--- client/src/components/protectedRoute.jsx
+++ client/src/components/protectedRoute.jsx
@@ -1,8 +1,11 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { UseAuth } from "@/context/AuthContext";
 
 
-export default function protectedRoute({ children }) {
+export default function protectedRoute({ role }) {
   const {user} = UseAuth();
-  return user ? children:<Navigate to='/login' />;
+  if (!user) return <Navigate to ="/login" />;
+  if (user&& user.role!==role) return <Navigate to='/'/>
+
+  return <Outlet />;
 }
